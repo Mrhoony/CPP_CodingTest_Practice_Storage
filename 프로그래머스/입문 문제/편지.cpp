b935e4c429b41d209848a10ade60bea9b7e3b6f1
Mrhoony <newyorkkimchis@gmail.com>
2025-06-01 22:00:58 +0900

    Create 편지.cpp

---
+++ 프로그래머스/입문 문제/편지.cpp
@@ -0,0 +1,16 @@
+/*
+ * string::size()
+ */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    
+    answer = message.size() * 2;
+    
+    return answer;
+}
